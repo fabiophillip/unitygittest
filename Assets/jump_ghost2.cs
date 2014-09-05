be2e3e3d0cb404e07297b7fce4926d6b3a2deee8
fabio phillip <fabiophillip@hotmail.com>
2014-09-05 13:27:19 -0300

    commit inicial projeto
    commit inicial projeto

---
+++ Assets/jump_ghost2.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class jump_ghost2 : MonoBehaviour 
+{
+	public Vector3 displacement;
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		//transform.Translate(displacement); FAZ PERSONAGEM ANDAR PRA DIREITA CASO VOCE ALTERE X NO DISPLACEMENT NO inspector do objeto
+		if (Input.GetKey (KeyCode.Space)) 
+		{
+			rigidbody2D.AddForce(new Vector2(0.0f,50.0f));
+		}
+	}
+	 
+}
